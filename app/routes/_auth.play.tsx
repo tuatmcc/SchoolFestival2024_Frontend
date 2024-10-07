e2a4e04402a2f23357fc81d03e055c64c0832268
thisagi <106305548+thisagi@users.noreply.github.com>
2024-10-07 18:59:24 +0900

    issue #11 QR code

--- app/routes/_auth.play.tsx
+++ app/routes/_auth.play.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useSession } from "~/hooks/useSession";
 import QRCode from "~/components/qrcode";
 
 export const meta: MetaFunction = () => {
@@ -6,11 +7,15 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Page() {
+	const session = useSession();
+
+	if(!session) return null
 	return (
 		<div className="p-4 font-sans">
 			<h1 className="text-3xl">QR code</h1>
 			<ul className="mt-4 list-disc space-y-2 pl-6">
-				<QRCode url="https://www.tuatmcc.com/" size={512} />
+				<QRCode url={session.user.id} size={512} />
+				<div>ID: {session.user.id}</div>
 			</ul>
 		</div>
 	);
