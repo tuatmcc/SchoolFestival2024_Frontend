69bc97f931829448c7eee1d73621b9859f74a649
NISHIZAWA Shuntaro <me@s2n.tech>
2024-10-11 23:33:27 +0900

    feat: Google Analyticsを追加

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 import "./tailwind.css";
 import { Suspense } from "react";
 import { Loading } from "./components/Loading";
+import { Partytown } from "@builder.io/partytown/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
@@ -15,6 +16,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Partytown debug forward={["dataLayer.push"]} />
+				{import.meta.env.VITE_GOOGLE_ANALYTICS_ID && (
+					<>
+						<script
+							async
+							src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}`}
+						/>
+						<script
+							// biome-ignore lint/security/noDangerouslySetInnerHtml: This is safe because the Google Analytics ID is a build-time constant
+							dangerouslySetInnerHTML={{
+								__html: `window.dataLayer||=[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}')`,
+							}}
+						/>
+					</>
+				)}
 				<Meta />
 				<Links />
 			</head>
