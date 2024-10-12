8b1f60b886abeae43b7d8a0e1707ed457cfc7172
NISHIZAWA Shuntaro <me@s2n.tech>
2024-10-12 22:33:44 +0900

    feat: profilesのテーブルを作成

--- app/libs/supabase.ts
+++ app/libs/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./database";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
 	import.meta.env.VITE_SUPABASE_URL,
 	import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
