228e58361b21ef9989e60ee2e8395788f68d5422
NISHIZAWA Shuntaro <me@s2n.tech>
2024-10-13 22:06:45 +0900

    feat: 対戦結果を保存するテーブルを定義

---
+++ supabase/migrations/20241013105417_add_matching.sql
@@ -0,0 +1,100 @@
+-- 対戦結果テーブル
+CREATE TABLE public.battle_results (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  start_at TIMESTAMPTZ NOT NULL,
+  end_at TIMESTAMPTZ NOT NULL,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+-- チームテーブル
+CREATE TABLE public.teams (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  battle_result_id UUID NOT NULL REFERENCES public.battle_results ON DELETE CASCADE,
+
+  is_win BOOLEAN NOT NULL DEFAULT FALSE,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+  -- 複合外部キー制約のために複合ユニーク制約を設定
+  UNIQUE (id, battle_result_id)
+);
+
+-- プレイヤーテーブル
+CREATE TABLE public.players (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  -- そのユーザーがアカウントを削除した場合はNULLにする
+  -- アカウント削除はできたほうが良いが、対戦成績は残しておきたいため。
+  -- 対戦成績は他ユーザーにも表示される可能性がある。
+  user_id UUID UNIQUE REFERENCES auth.users ON DELETE SET NULL,
+  team_id UUID NOT NULL REFERENCES public.teams ON DELETE CASCADE,
+  battle_result_id UUID NOT NULL REFERENCES public.battle_results ON DELETE CASCADE,
+
+  score INTEGER NOT NULL,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+  -- ユーザーは対戦結果に対して1つのプレイヤーしか存在できない
+  UNIQUE (user_id, battle_result_id),
+  -- team_idから参照されるbattle_result_idと一致することを保証する
+  FOREIGN KEY (team_id, battle_result_id) REFERENCES public.teams (id, battle_result_id) ON DELETE CASCADE
+);
+
+
+-- データ更新時に更新日時を更新するトリガー
+CREATE TRIGGER
+  update_battle_result_timestamp
+  BEFORE UPDATE ON public.battle_results
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.update_timestamp();
+CREATE TRIGGER
+  update_team_timestamp
+  BEFORE UPDATE ON public.teams
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.update_timestamp();
+CREATE TRIGGER
+  update_player_timestamp
+  BEFORE UPDATE ON public.players
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.update_timestamp();
+
+
+-- パフォーマンス向上のため、インデックスを設定
+CREATE INDEX idx_players_user_id ON public.players (user_id);
+CREATE INDEX idx_players_team_id ON public.players (team_id);
+CREATE INDEX idx_players_battle_result_id ON public.players (battle_result_id);
+
+CREATE INDEX idx_teams_battle_result_id ON public.teams (battle_result_id);
+
+
+-- RLSを有効化
+ALTER TABLE public.battle_results ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.teams ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.players ENABLE ROW LEVEL SECURITY;
+
+
+-- 参照のみ全ユーザーに許可
+CREATE POLICY
+  "Can read everyone's battle result"
+  ON public.battle_results
+  FOR SELECT
+  TO authenticated, anon
+  USING ( true );
+CREATE POLICY
+  "Can read everyone's team"
+  ON public.teams
+  FOR SELECT
+  TO authenticated, anon
+  USING ( true );
+CREATE POLICY
+  "Can read everyone's player"
+  ON public.players
+  FOR SELECT
+  TO authenticated, anon
+  USING ( true );
