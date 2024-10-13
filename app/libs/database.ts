707b7d48ae7292b9957e2352147fbc064a164937
NISHIZAWA Shuntaro <me@s2n.tech>
2024-10-13 12:26:54 +0900

    feat: `profiles`テーブルに作成日時と更新日時のカラムを追加

--- app/libs/database.ts
+++ app/libs/database.ts
@@ -36,18 +36,24 @@ export type Database = {
     Tables: {
       profiles: {
         Row: {
+          created_at: string
           display_name: string
           id: string
+          updated_at: string
           user_id: string
         }
         Insert: {
+          created_at?: string
           display_name: string
           id?: string
+          updated_at?: string
           user_id: string
         }
         Update: {
+          created_at?: string
           display_name?: string
           id?: string
+          updated_at?: string
           user_id?: string
         }
         Relationships: [
