73b149b2f776e3890294c2c474a134802e995f4b
NISHIZAWA Shuntaro <me@s2n.tech>
2024-10-21 19:10:02 +0900

    feat: ProfileCardコンポーネントを追加

---
+++ app/components/ProfileCard.tsx
@@ -0,0 +1,49 @@
+import type { Profile } from "~/features/profile/Profile";
+import { cn } from "~/libs/utils";
+import { Button } from "./Button";
+import { Card } from "./Card";
+
+export interface ProfileCardProps {
+	profile: Profile;
+	onClickEdit?: () => void;
+	classNames?: string;
+}
+
+export function ProfileCard({
+	profile,
+	onClickEdit,
+	classNames,
+}: ProfileCardProps) {
+	return (
+		<Card className={cn("grid w-full max-w-screen-sm gap-4 p-4", classNames)}>
+			<div className="flex min-w-0 items-center justify-between gap-4 px-2">
+				<span className="truncate text-2xl drop-shadow-base">
+					{profile.displayName}
+				</span>
+				<Button className="flex-shrink-0" onClick={onClickEdit}>
+					編集
+				</Button>
+			</div>
+			<div className="grid grid-cols-[1fr_auto_1fr_auto_1fr] items-center drop-shadow-base">
+				<div className="grid justify-items-center gap-1 text-center">
+					<span>順位</span>
+					<span className="text-2xl">
+						{profile.rank ? `${profile.rank}位` : "-"}
+					</span>
+				</div>
+				<hr className="h-2/3 w-0 rounded-full border-2 border-white" />
+				<div className="grid justify-items-center gap-1 text-center">
+					<span>ハイスコア</span>
+					<span className="text-2xl">
+						{profile.highScore ? profile.highScore : "-"}
+					</span>
+				</div>
+				<hr className="h-2/3 w-0 rounded-full border-2 border-white" />
+				<div className="grid justify-items-center gap-1 text-center">
+					<span>プレイ回数</span>
+					<span className="text-2xl">{profile.playCount}</span>
+				</div>
+			</div>
+		</Card>
+	);
+}
