b34e7f1df1dc35a09e513ca015b54f01e887a3bd
toufu-24 <120612884+toufu-24@users.noreply.github.com>
2024-10-23 01:25:15 +0900

    feat:すべてのモデルのボタンを追加

--- app/routes/_auth.edit.tsx
+++ app/routes/_auth.edit.tsx
@@ -1,10 +1,28 @@
 import type { ReactNode } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Heading } from "~/components/Heading";
 import { ModelViewer } from "~/components/ModelLoad";
 
 export default function Page(): ReactNode {
 	const [modelPath, setModelPath] = useState("/models/web_asuka.glb");
+	const [models, setModels] = useState<string[]>([]);
+
+	useEffect(() => {
+		// モデルファイルを取得する関数
+		const fetchModels = async () => {
+			// 自動取得したかったけど，わからんかったのでハードコーディング
+			const modelFiles = [
+				"/models/web_asuka.glb",
+				"/models/web_jiraichan.glb",
+				"/models/web_kaiju.glb",
+				"/models/web_necochan.glb",
+				"/models/web_sushong.glb",
+			];
+			setModels(modelFiles);
+		};
+
+		fetchModels();
+	}, []);
 
 	return (
 		<div
@@ -15,12 +33,12 @@ export default function Page(): ReactNode {
 				<Heading>キャラ編集</Heading>
 				{/* ModelViewerを中央に配置するためのスタイルを追加 */}
 				<ModelViewer modelPath={modelPath} />
-				<button
-					type="button"
-					onClick={() => setModelPath("/models/web_jiraichan.glb")}
-				>
-					別のキャラを表示
-				</button>
+				{/* モデルごとのボタンを生成 */}
+				{models.map((model) => (
+					<button key={model} type="button" onClick={() => setModelPath(model)}>
+						{model.split("/").pop()} {/* ファイル名を表示 */}
+					</button>
+				))}
 			</main>
 		</div>
 	);
