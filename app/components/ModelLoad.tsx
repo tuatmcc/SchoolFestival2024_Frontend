5493e58999135c5c7a8d3a3fd893322f4a4094fa
toufu-24 <120612884+toufu-24@users.noreply.github.com>
2024-10-23 01:26:57 +0900

    fix:表示をある程度修正(小さい画面でまだバグる)

--- app/components/ModelLoad.tsx
+++ app/components/ModelLoad.tsx
@@ -1,6 +1,6 @@
 import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { type ReactNode, useRef, useState } from "react";
+import { type ReactNode, useRef } from "react";
 import type { Group } from "three";
 
 // 3Dモデルを表示するためのコンポーネント
@@ -25,36 +25,24 @@ function Model({ path }: { path: string }): ReactNode {
 // モデルのパスを受け取って、それを表示する
 type ModelViewerProps = {
 	modelPath: string; // モデルのパス
-	height?: number | string; // 高さ
-	width?: number | string; // 幅
 };
-export function ModelViewer({
-	modelPath,
-	height = "80vh",
-}: ModelViewerProps): ReactNode {
+export function ModelViewer({ modelPath }: ModelViewerProps): ReactNode {
 	return (
 		<div>
 			{/* 3Dモデルを表示するためのCanvasエリア */}
-			<div style={{ height: height }}>
+			<div style={{ height: "60vh" }}>
 				<Canvas
-					camera={{ position: [0, 0, 5], fov: 50 }} // カメラの初期位置と視野角を設定
+					camera={{
+						position: [0, 0, 4],
+						fov: 30,
+					}} // カメラの初期位置と視野角を設定
 				>
 					{/* 環境を設定*/}
 					<Environment preset="studio" />
 					{/* 環境光を追加（全体的に均一な光を当てる） */}
 					<ambientLight intensity={0.5} />
-					{/* スポットライトを追加（シーンに方向性のある光を当てる） */}
-					<spotLight
-						position={[10, 10, 10]} // スポットライトの位置
-						angle={0.15} // スポットライトの角度
-						penumbra={1} // スポットライトの影のぼかし具合
-						decay={0} // 光の減衰を設定
-						intensity={3} // 光の強さを設定
-					/>
-
 					{/* GLBモデルの読み込みと表示 */}
 					<Model path={modelPath} />
-
 					{/* カメラコントロールの追加（ユーザーが自由にカメラを操作できるようにする） */}
 					<OrbitControls makeDefault />
 				</Canvas>
