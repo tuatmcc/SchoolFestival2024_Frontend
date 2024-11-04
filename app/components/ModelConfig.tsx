be1cc3a935a6fd207d33ff02208ef59534812aee
NISHIZAWA Shuntaro <me@s2n.tech>
2024-11-05 03:43:09 +0900

    feat: モデル切り替え時にDB側も更新されるように

--- app/components/ModelConfig.tsx
+++ app/components/ModelConfig.tsx
@@ -1,63 +1,34 @@
-import { useEffect, useState } from "react";
+import { MODEL_LIST } from "~/features/profile/Profile";
+import type { CharacterSetting, Model } from "~/features/profile/Profile";
 
-interface ModelListProps {
-	onModelSelect: (modelPath: string) => void;
-	updateVisibility: (key: string, value: boolean) => void;
-	updateColor: (key: string, value: string) => void;
+interface ModelConfigProps {
+	characterSetting: CharacterSetting;
+	onModelSelect?: (model: Model) => void;
 }
 
 export function ModelConfig({
+	characterSetting,
 	onModelSelect,
-	updateVisibility,
-	updateColor,
-}: ModelListProps) {
-	const [models, setModels] = useState<string[]>([]);
-	const [containerWidth, setContainerWidth] = useState(0);
-	const [selectedColors, setSelectedColors] = useState<Record<string, string>>(
-		{},
-	);
-
-	useEffect(() => {
-		const fetchModels = async () => {
-			const modelFiles = [
-				"/models/web_asuka.glb",
-				"/models/web_jiraichan.glb",
-				"/models/web_kaiju.glb",
-				"/models/web_necochan.glb",
-				"/models/web_sushong.glb",
-			];
-			setModels(modelFiles);
-		};
-
-		fetchModels();
-
-		const updateContainerWidth = () => {
-			const containerRef = document.getElementById("model-list-container");
-			if (containerRef) {
-				setContainerWidth(containerRef.offsetWidth);
-			}
-		};
-
-		updateContainerWidth();
-		window.addEventListener("resize", updateContainerWidth);
-
-		return () => window.removeEventListener("resize", updateContainerWidth);
-	}, []);
-
-	// ボタンの幅を計算
-	const buttonWidth = `${Math.floor(containerWidth / models.length) - 16}px`;
-	// 表示非表示のキー
-	const visibilityKeys = [
-		"accessoryeyepatch",
-		"accessoryglasses",
-		"goggle",
-		"accessoryhalo",
-	];
-
+}: ModelConfigProps) {
 	return (
 		<div className="mx-auto w-full max-w-screen-sm sm:px-4">
+			<div className="flex justify-between">
+				{MODEL_LIST.map((model) => (
+					<label key={model}>
+						<input
+							type="radio"
+							name="model"
+							value={model}
+							checked={model === characterSetting.character}
+							onChange={() => onModelSelect?.(model)}
+						/>
+						<span>{model}</span>
+					</label>
+				))}
+			</div>
+
 			{/* モデル選択ボタン */}
-			<div
+			{/* <div
 				id="model-list-container"
 				className="mb-4 flex flex-wrap justify-center space-x-2"
 			>
@@ -72,9 +43,9 @@ export function ModelConfig({
 						{model.split("/").pop()?.split("_")[1].split(".")[0]}
 					</button>
 				))}
-			</div>
+			</div> */}
 			{/* 表示非表示ボタン */}
-			<div className="flex flex-col items-center">
+			{/* <div className="flex flex-col items-center">
 				{visibilityKeys.map((key) => (
 					<div key={key} className="flex items-center space-x-2">
 						<span>{key}</span>
@@ -94,9 +65,9 @@ export function ModelConfig({
 						</button>
 					</div>
 				))}
-			</div>
+			</div> */}
 			{/* 色変更ボタン */}
-			<div className="flex items-center justify-center space-x-2">
+			{/* <div className="flex items-center justify-center space-x-2">
 				<span className="font-medium">Hair Color:</span>
 				<input
 					type="color"
@@ -108,7 +79,7 @@ export function ModelConfig({
 					className="h-8 w-8 cursor-pointer rounded border border-gray-300"
 					title="髪の色を変更"
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 }
--- app/routes/_auth.edit.tsx
+++ app/routes/_auth.edit.tsx
@@ -5,26 +5,29 @@ import { getMeshColor } from "~/components/ModelColor";
 import { ModelConfig } from "~/components/ModelConfig";
 import { ModelViewer } from "~/components/ModelLoad";
 import { getMeshVisibility } from "~/components/ModelVisibility";
+import type { Model } from "~/features/profile/Profile";
 import { useMyProfile } from "~/features/profile/useMyProfile";
 
 export const meta: MetaFunction = () => [{ title: "キャラ編集 | RicoShot" }];
 
 export default function Page(): ReactNode {
-	const { myProfile } = useMyProfile();
+	const { myProfile, updateCharacterSetting } = useMyProfile();
 	if (!myProfile) return null;
 
-	const [modelPath, setModelPath] = useState("/models/web_asuka.glb");
-	const { meshVisibility, updateVisibility } = getMeshVisibility();
-	const { meshColor, updateVisibility: updateColor } = getMeshColor();
+	const handleModelSelect = (model: Model) => {
+		updateCharacterSetting({
+			...myProfile.characterSetting,
+			character: model,
+		});
+	};
 
 	return (
 		<div className="min-h-dvh w-full" style={{ viewTransitionName: "main" }}>
 			<main className="grid w-full gap-y-4">
 				<ModelViewer characterSetting={myProfile.characterSetting} />
 				<ModelConfig
-					onModelSelect={setModelPath}
-					updateVisibility={updateVisibility}
-					updateColor={updateColor}
+					characterSetting={myProfile.characterSetting}
+					onModelSelect={handleModelSelect}
 				/>
 			</main>
 		</div>
