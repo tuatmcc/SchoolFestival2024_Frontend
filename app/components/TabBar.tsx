5fc779cc14aab848049876c5d8d2429ac03315db
thisagi <106305548+thisagi@users.noreply.github.com>
2024-11-05 15:04:31 +0900

    fix: lint

--- app/components/TabBar.tsx
+++ app/components/TabBar.tsx
@@ -1,9 +1,6 @@
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
-import * as Tabs from '@radix-ui/react-tabs';
+import * as Tabs from "@radix-ui/react-tabs";
 import { Link } from "@remix-run/react";
-import type { ReactNode } from "react";
 import { cn } from "~/libs/utils";
-import { BACKGROUND_PATTERN_ID, BUTTON_BG_PATTERN_ID } from "./Patterns";
 
 const LINKS = [
 	{
@@ -31,8 +28,11 @@ export function TabBar({ path, className }: TabBarProps) {
 	return (
 		<Tabs.Root
 			value={LINKS[index].href}
-			className={cn("relative w-full max-w-screen-sm bg-gray-600 overflow-hidden", className)}
-			style={{ borderRadius: '10px 10px 0 0' }}
+			className={cn(
+				"relative w-full max-w-screen-sm overflow-hidden bg-gray-600",
+				className,
+			)}
+			style={{ borderRadius: "10px 10px 0 0" }}
 		>
 			<Tabs.List className="flex">
 				{LINKS.map((link) => (
@@ -40,10 +40,10 @@ export function TabBar({ path, className }: TabBarProps) {
 						key={link.href}
 						value={link.href}
 						className={cn(
-							"flex-1 py-3 text-center transition-transform md:text-xl text-white bg-gray-600",
-							{ "bg-cyan-400": link.href === path }
+							"flex-1 bg-gray-600 py-3 text-center text-white transition-transform md:text-xl",
+							{ "bg-cyan-400": link.href === path },
 						)}
-						style={{borderRadius: path === link.href ? '10px 10px 0 0' : '0'}}
+						style={{ borderRadius: path === link.href ? "10px 10px 0 0" : "0" }}
 					>
 						<Link to={link.href} viewTransition>
 							{link.label}
