b221aeecac13e514dddc12fed49ed9845c4cf21f
NISHIZAWA Shuntaro <me@s2n.tech>
2024-11-07 00:52:24 +0900

    feat: ホーム画面にランキングを追加

--- app/routes/_index/SignUp.tsx
+++ app/routes/_index/SignUp.tsx
@@ -1,11 +1,11 @@
 import type { ReactNode } from "react";
-import { LogginCard } from "~/components/LogginCard";
+import { LoginCard } from "~/components/LoginCard";
 
 export function SignUp(): ReactNode {
 	return (
 		<div className="grid justify-items-center gap-8">
 			<img src="/logo.svg" alt="RicoShot" className="w-full" />
-			<LogginCard className="w-full rotate-2" />
+			<LoginCard className="w-full rotate-2" />
 		</div>
 	);
 }
